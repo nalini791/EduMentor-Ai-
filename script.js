b29045eb54c4a5dcab86b6496bbd3593c0eed5c6
nalini791 <pradhannalini791@gmail.com>
2026-02-18 11:31:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+body {
+  font-family: Arial;
+  text-align: center;
+  background: #f5f5f5;
+}
+
+#chat {
+  height: 300px;
+  width: 90%;
+  margin: auto;
+  background: white;
+  border: 1px solid gray;
+  overflow: auto;
+  padding: 10px;
+}
